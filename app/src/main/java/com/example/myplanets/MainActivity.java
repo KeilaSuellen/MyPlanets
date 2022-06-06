7914874e0e9e641832739e808a4b1db802c6032b
KeilaSuellen <keilasuellen02@gmail.com>
2022-06-06 20:23:09 -0300

    Lista planetas

--- app/src/main/java/com/example/myplanets/MainActivity.java
+++ app/src/main/java/com/example/myplanets/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.myplanets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Declarar a lista
+    ListView listaPlanetas;
+
+    //criando os vetores com os dados para a lista
+    String nomePlanetas[] = {"Jupiter", "Lua", "Marte", "Mercurio", "Netuno", "Plutao", "Saturno", "Sol", "Terra", "Urano", "Venus"};
+    int imagemPlanetas[] = {R.drawable.jupiter, R.drawable.lua, R.drawable.marte, R.drawable.mercurio, R.drawable.netuno, R.drawable.plutao, R.drawable.saturno,
+            R.drawable.sol, R.drawable.terra, R.drawable.urano, R.drawable.venus};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaPlanetas = findViewById(R.id.isListaPlanetas);
+
+        MyAdapter adapter = new MyAdapter();
+
+        listaPlanetas.setAdapter(adapter);
+    }
+
+    //Criando a classe interna - inner class
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return imagemPlanetas.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            //Declarar os componentes do  modelo que serao utilizados na lista
+            ImageView imgPlanetas;
+            TextView txtPlanetas;
+            CardView cardPlanetas;
+
+            View view1 = getLayoutInflater().inflate(R.layout.modelo_planetas,null);
+            txtPlanetas =view1.findViewById(R.id.txtModeloPlaneta);
+            imgPlanetas = view1.findViewById(R.id.imgModeloPlaneta);
+
+            //implemnetar os metodos nos componentes
+            txtPlanetas.setText(nomePlanetas[i]);
+            imgPlanetas.setImageResource(imagemPlanetas[i]);
+
+            return view1;
+        }
     }
 }
